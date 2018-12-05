c6b184cd13aac6063c213b9fbdf930e0991d1bb6
arthurlgk@outlook.com <Nswe255956>
2018-12-04 18:29:35 -0600

    timer added

--- app/src/main/java/com/example/arthu/csfinal/MainActivity.java
+++ app/src/main/java/com/example/arthu/csfinal/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.arthu.csfinal;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView countdownText;
+    private Button countdownButton;
+
+
+    private CountDownTimer countdownTimer;
+    private long timeLeftInMilliseconds = 600000; //10 mins
+    private boolean timeRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countdownText = findViewById(R.id.countdown_text);
+        countdownButton = findViewById(R.id.countdown_button);
+
+
+        countdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startStop();
+            }
+        });
+
+        TextView slogan = findViewById(R.id.slogan);
+        slogan.setText("Stay safe and take care!");
+        TextView phoneNumberTitle = findViewById(R.id.pntitle);
+        phoneNumberTitle.setText("Phone#:");
+    }
+    public void startStop() {
+        if (timeRunning) {
+            stopTimer();
+        } else {
+            startTimer();
+        }
+    }
+
+    public void startTimer() {
+        countdownTimer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMilliseconds = millisUntilFinished;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+
+        countdownButton.setText("PAUSE");
+        timeRunning = true;
+    }
+
+    public void stopTimer() {
+        countdownTimer.cancel();
+        countdownButton.setText("Start");
+        timeRunning = false;
+    }
+
+    public void updateTimer() {
+        int minutes = (int) timeLeftInMilliseconds / 60000;
+        int seconds = (int) timeLeftInMilliseconds % 60000 / 1000;
+
+        String timeLeftText;
+
+        timeLeftText = " " + minutes;
+        timeLeftText += ":";
+        if (seconds < 10) {
+            timeLeftText += "0";
+        }
+        timeLeftText += seconds;
+
+        countdownText.setText(timeLeftText);
     }
 }
