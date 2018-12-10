623ff187dcb5ddecc966b5918c048ec656f1b128
arthurlgk@outlook.com <Nswe255956>
2018-12-09 21:37:54 -0600

    notification added

--- app/src/main/java/com/example/arthu/csfinal/MainActivity.java
+++ app/src/main/java/com/example/arthu/csfinal/MainActivity.java
@@ -1,54 +1,66 @@
 package com.example.arthu.csfinal;
 
-import android.app.NotificationManager;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static com.example.arthu.csfinal.App.CHANNEL_1_ID;
+
 public class MainActivity extends AppCompatActivity {
     private TextView countdownText;
     private Button countdownButton;
+    private Button resumeButton;
 
 
     private CountDownTimer countdownTimer;
     private long timeLeftInMilliseconds = 600000; //10 mins
     private boolean timeRunning;
 
-    NotificationCompat.Builder notification;
-    private static final int uniqueID = 83481;
+    private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        notificationManager = NotificationManagerCompat.from(this);
+
         countdownText = findViewById(R.id.countdown_text);
         countdownButton = findViewById(R.id.countdown_button);
+        resumeButton = findViewById(R.id.countdown_resume);
 
         countdownButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startStop();
+
             }
         }); // ButtonClick to start or stop the timer
+
+        updateTimer();
+
+        resumeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resumeTimer();
+            }
+        });
 
         TextView slogan = findViewById(R.id.slogan);
         slogan.setText("Stay safe and take care!");
         TextView phoneNumberTitle = findViewById(R.id.pntitle);
         phoneNumberTitle.setText("Phone#:");
 
-        notification = new NotificationCompat.Builder(this);
-        notification.setAutoCancel(true);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,PendingIntent.FLAG_UPDATE_CURRENT);
-        notification.setContentIntent(pendingIntent);
     }
 
     /**
@@ -60,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
             stopTimer();
         } else {
             startTimer();
+
         }
     }
 
@@ -71,15 +84,8 @@ public class MainActivity extends AppCompatActivity {
             public void onTick(long millisUntilFinished) {
                 timeLeftInMilliseconds = millisUntilFinished;
                 updateTimer();
-                if (timeLeftInMilliseconds == 1) {
-                    notification.setSmallIcon(R.mipmap.ic_launcher);
-                    notification.setTicker("ALERT");
-                    notification.setWhen(System.currentTimeMillis());
-                    notification.setContentTitle("Alert!");
-                    notification.setContentText("If you feel safe, please return to stop the timer.");
-
-                    NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                    nm.notify(uniqueID, notification.build());
+                if (timeLeftInMilliseconds < 60000) {
+                    sendNotification();
                 }
             }
 
@@ -99,6 +105,16 @@ public class MainActivity extends AppCompatActivity {
         timeRunning = false;
     }
 
+    public void resumeTimer() {
+        countdownTimer.cancel();
+        timeRunning = false;
+        String timeLeftText;
+
+        timeLeftText = " 10:00";
+        timeLeftInMilliseconds = 600000;
+        countdownText.setText(timeLeftText);
+    }
+
     public void updateTimer() {
         int minutes = (int) timeLeftInMilliseconds / 60000;
         int seconds = (int) timeLeftInMilliseconds % 60000 / 1000;
@@ -113,7 +129,26 @@ public class MainActivity extends AppCompatActivity {
         timeLeftText += seconds;
 
         countdownText.setText(timeLeftText);
+
     }
 
+    public void sendNotification() {
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
 
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_icon)
+                .setContentTitle("ALERT!!")
+                .setContentText("If you feel safe, please return to app to stop the timer.")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setColor(Color.RED)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .build();
+        notificationManager.notify(1, notification);
+
+
+
+    }
 }
